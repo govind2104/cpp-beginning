75c82af4c490b52fb74f9a180a6801715f6a9b86
Govind Kumar <govind09bgs@gmail.com>
2026-07-05 22:53:08 +0530

    add do_while02.cpp

--- 05_loops/break.cpp
+++ 05_loops/break.cpp
@@ -1,24 +1,13 @@
-// write a program that keeps serving tea until the user
-//  says they've had enough (input 'stop'). use a break
-//   statement to exit the loop when the user types 'stop'
 #include<iostream>
 #include<string.h>
-using namespace std;
-
 int main(){
 
     string response;
- do{
-    cout<<"Do you want a cup of tea. (yes/no),\n Enter 'stop' if you don't want further cup  of tea to  be delivered.";
-    getline(cin,response);
-
-   
-        cout<<"please wait, your cup of tea will be served shortly."<<endl;
-  } while(response!="stop");
- cout<<"No more tea cups will be served.\n Thankyou for your order."<<endl;
+    while(true){
+        cout<<"do you want a cup of tea. (yes/no),\n Enter 'stop' if you don't want further cup  of tea to  be delivered.";
+        getline(cin,response);
+        
+        if(response=="stop"){
+            break;}
     
-
-
-
-    return 0;
 }
