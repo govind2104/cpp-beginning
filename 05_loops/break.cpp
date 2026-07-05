a5a26d546421856adcb1f39d4f19b2ee0e3d6480
Govind Kumar <govind09bgs@gmail.com>
2026-07-05 23:12:00 +0530

    changes

--- 05_loops/break.cpp
+++ 05_loops/break.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
-#include<string.h>
+#include<string>
+using namespace std;
+
 int main(){
 
     string response;
+
     while(true){
         cout<<"do you want a cup of tea. (yes/no),\n Enter 'stop' if you don't want further cup  of tea to  be delivered.";
         getline(cin,response);
         
         if(response=="stop"){
             break;}
+
+            cout<<"here is your another cup of tea."<<endl;
     
+}
+return 0;
 }
